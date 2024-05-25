f894198df7b2b2f47758ebd5f841887844456058
StarSer <androidemoc@gmail.com>
2024-05-25 00:10:14 -0700

    SE VAN A MORIR

--- BaseDeDatosBOA/Form1.Designer.cs
+++ BaseDeDatosBOA/Form1.Designer.cs
@@ -40,10 +40,17 @@
             this.BtnClientes = new FontAwesome.Sharp.IconButton();
             this.BtnProcesadores = new FontAwesome.Sharp.IconButton();
             this.BtnVenta = new FontAwesome.Sharp.IconButton();
-            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.BtnInicio = new Guna.UI2.WinForms.Guna2PictureBox();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.gunaPanelFondo = new Guna.UI2.WinForms.Guna2Panel();
+            this.iconTitulo = new FontAwesome.Sharp.IconPictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
             this.Gunapanelizquierdo.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnInicio)).BeginInit();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconTitulo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Gunapanelizquierdo
@@ -76,9 +83,10 @@
             this.BtnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnInventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnInventario.ForeColor = System.Drawing.Color.White;
-            this.BtnInventario.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnInventario.IconColor = System.Drawing.Color.Black;
+            this.BtnInventario.IconChar = FontAwesome.Sharp.IconChar.List;
+            this.BtnInventario.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnInventario.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnInventario.IconSize = 40;
             this.BtnInventario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnInventario.Location = new System.Drawing.Point(0, 106);
             this.BtnInventario.Name = "BtnInventario";
@@ -96,9 +104,10 @@
             this.BtnTarjetaMadre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnTarjetaMadre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnTarjetaMadre.ForeColor = System.Drawing.Color.White;
-            this.BtnTarjetaMadre.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnTarjetaMadre.IconColor = System.Drawing.Color.Black;
+            this.BtnTarjetaMadre.IconChar = FontAwesome.Sharp.IconChar.Inbox;
+            this.BtnTarjetaMadre.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnTarjetaMadre.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnTarjetaMadre.IconSize = 40;
             this.BtnTarjetaMadre.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnTarjetaMadre.Location = new System.Drawing.Point(0, 279);
             this.BtnTarjetaMadre.Name = "BtnTarjetaMadre";
@@ -116,9 +125,10 @@
             this.BtnAlmacenamiento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnAlmacenamiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnAlmacenamiento.ForeColor = System.Drawing.Color.White;
-            this.BtnAlmacenamiento.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnAlmacenamiento.IconColor = System.Drawing.Color.Black;
+            this.BtnAlmacenamiento.IconChar = FontAwesome.Sharp.IconChar.FloppyDisk;
+            this.BtnAlmacenamiento.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnAlmacenamiento.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnAlmacenamiento.IconSize = 40;
             this.BtnAlmacenamiento.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnAlmacenamiento.Location = new System.Drawing.Point(0, 452);
             this.BtnAlmacenamiento.Name = "BtnAlmacenamiento";
@@ -136,9 +146,10 @@
             this.BtnEmpleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnEmpleados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnEmpleados.ForeColor = System.Drawing.Color.White;
-            this.BtnEmpleados.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnEmpleados.IconColor = System.Drawing.Color.Black;
+            this.BtnEmpleados.IconChar = FontAwesome.Sharp.IconChar.AddressCard;
+            this.BtnEmpleados.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnEmpleados.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnEmpleados.IconSize = 40;
             this.BtnEmpleados.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnEmpleados.Location = new System.Drawing.Point(0, 192);
             this.BtnEmpleados.Name = "BtnEmpleados";
@@ -156,9 +167,10 @@
             this.BtnGrafica.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnGrafica.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnGrafica.ForeColor = System.Drawing.Color.White;
-            this.BtnGrafica.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnGrafica.IconColor = System.Drawing.Color.Black;
+            this.BtnGrafica.IconChar = FontAwesome.Sharp.IconChar.Image;
+            this.BtnGrafica.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnGrafica.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnGrafica.IconSize = 40;
             this.BtnGrafica.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnGrafica.Location = new System.Drawing.Point(0, 366);
             this.BtnGrafica.Name = "BtnGrafica";
@@ -175,9 +187,10 @@
             this.BtnFuenteDePoder.FlatAppearance.BorderSize = 0;
             this.BtnFuenteDePoder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnFuenteDePoder.ForeColor = System.Drawing.Color.White;
-            this.BtnFuenteDePoder.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnFuenteDePoder.IconColor = System.Drawing.Color.Black;
+            this.BtnFuenteDePoder.IconChar = FontAwesome.Sharp.IconChar.Plug;
+            this.BtnFuenteDePoder.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnFuenteDePoder.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnFuenteDePoder.IconSize = 40;
             this.BtnFuenteDePoder.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnFuenteDePoder.Location = new System.Drawing.Point(0, 497);
             this.BtnFuenteDePoder.Name = "BtnFuenteDePoder";
@@ -195,9 +208,10 @@
             this.BtnComputadoras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnComputadoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnComputadoras.ForeColor = System.Drawing.Color.White;
-            this.BtnComputadoras.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnComputadoras.IconColor = System.Drawing.Color.Black;
+            this.BtnComputadoras.IconChar = FontAwesome.Sharp.IconChar.Computer;
+            this.BtnComputadoras.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnComputadoras.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnComputadoras.IconSize = 40;
             this.BtnComputadoras.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnComputadoras.Location = new System.Drawing.Point(0, 237);
             this.BtnComputadoras.Name = "BtnComputadoras";
@@ -215,9 +229,10 @@
             this.BtnRam.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnRam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnRam.ForeColor = System.Drawing.Color.White;
-            this.BtnRam.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnRam.IconColor = System.Drawing.Color.Black;
+            this.BtnRam.IconChar = FontAwesome.Sharp.IconChar.Memory;
+            this.BtnRam.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnRam.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnRam.IconSize = 40;
             this.BtnRam.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnRam.Location = new System.Drawing.Point(0, 410);
             this.BtnRam.Name = "BtnRam";
@@ -235,9 +250,10 @@
             this.BtnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnClientes.ForeColor = System.Drawing.Color.White;
-            this.BtnClientes.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnClientes.IconColor = System.Drawing.Color.Black;
+            this.BtnClientes.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.BtnClientes.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnClientes.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnClientes.IconSize = 40;
             this.BtnClientes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnClientes.Location = new System.Drawing.Point(0, 150);
             this.BtnClientes.Name = "BtnClientes";
@@ -255,9 +271,10 @@
             this.BtnProcesadores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnProcesadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnProcesadores.ForeColor = System.Drawing.Color.White;
-            this.BtnProcesadores.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.BtnProcesadores.IconColor = System.Drawing.Color.Black;
+            this.BtnProcesadores.IconChar = FontAwesome.Sharp.IconChar.Microchip;
+            this.BtnProcesadores.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnProcesadores.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnProcesadores.IconSize = 40;
             this.BtnProcesadores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BtnProcesadores.Location = new System.Drawing.Point(0, 324);
             this.BtnProcesadores.Name = "BtnProcesadores";
@@ -275,8 +292,8 @@
             this.BtnVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnVenta.ForeColor = System.Drawing.Color.White;
-            this.BtnVenta.IconChar = FontAwesome.Sharp.IconChar.List;
-            this.BtnVenta.IconColor = System.Drawing.Color.Black;
+            this.BtnVenta.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart;
+            this.BtnVenta.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.BtnVenta.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.BtnVenta.IconSize = 40;
             this.BtnVenta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -289,19 +306,23 @@
             this.BtnVenta.UseVisualStyleBackColor = false;
             this.BtnVenta.Click += new System.EventHandler(this.BtnVenta_Click);
             // 
-            // guna2PictureBox1
+            // BtnInicio
             // 
-            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.guna2PictureBox1.Name = "guna2PictureBox1";
-            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
-            this.guna2PictureBox1.Size = new System.Drawing.Size(217, 64);
-            this.guna2PictureBox1.TabIndex = 1;
-            this.guna2PictureBox1.TabStop = false;
+            this.BtnInicio.BackColor = System.Drawing.Color.Transparent;
+            this.BtnInicio.Location = new System.Drawing.Point(0, 0);
+            this.BtnInicio.Name = "BtnInicio";
+            this.BtnInicio.ShadowDecoration.Parent = this.BtnInicio;
+            this.BtnInicio.Size = new System.Drawing.Size(217, 64);
+            this.BtnInicio.TabIndex = 1;
+            this.BtnInicio.TabStop = false;
+            this.BtnInicio.Click += new System.EventHandler(this.BtnInicio_Click_1);
             // 
             // guna2Panel1
             // 
             this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.Controls.Add(this.iconPictureBox1);
+            this.guna2Panel1.Controls.Add(this.lblTitulo);
+            this.guna2Panel1.Controls.Add(this.iconTitulo);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.guna2Panel1.ForeColor = System.Drawing.Color.Transparent;
             this.guna2Panel1.Location = new System.Drawing.Point(217, 0);
@@ -309,6 +330,56 @@
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
             this.guna2Panel1.Size = new System.Drawing.Size(1047, 67);
             this.guna2Panel1.TabIndex = 2;
+            this.guna2Panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseDown);
+            // 
+            // gunaPanelFondo
+            // 
+            this.gunaPanelFondo.BackColor = System.Drawing.Color.Transparent;
+            this.gunaPanelFondo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gunaPanelFondo.Location = new System.Drawing.Point(217, 67);
+            this.gunaPanelFondo.Name = "gunaPanelFondo";
+            this.gunaPanelFondo.ShadowDecoration.Parent = this.gunaPanelFondo;
+            this.gunaPanelFondo.Size = new System.Drawing.Size(1047, 614);
+            this.gunaPanelFondo.TabIndex = 3;
+            // 
+            // iconTitulo
+            // 
+            this.iconTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.iconTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
+            this.iconTitulo.IconChar = FontAwesome.Sharp.IconChar.House;
+            this.iconTitulo.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
+            this.iconTitulo.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconTitulo.IconSize = 34;
+            this.iconTitulo.Location = new System.Drawing.Point(15, 18);
+            this.iconTitulo.Name = "iconTitulo";
+            this.iconTitulo.Size = new System.Drawing.Size(41, 34);
+            this.iconTitulo.TabIndex = 0;
+            this.iconTitulo.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(62, 25);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(46, 20);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Inicio";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 41;
+            this.iconPictureBox1.Location = new System.Drawing.Point(994, 12);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(41, 45);
+            this.iconPictureBox1.TabIndex = 3;
+            this.iconPictureBox1.TabStop = false;
+            this.iconPictureBox1.Click += new System.EventHandler(this.iconPictureBox1_Click);
             // 
             // Form1
             // 
@@ -316,15 +387,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::BaseDeDatosBOA.Properties.Resources.hola__1_;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.gunaPanelFondo);
             this.Controls.Add(this.guna2Panel1);
-            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.BtnInicio);
             this.Controls.Add(this.Gunapanelizquierdo);
             this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Gunapanelizquierdo.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnInicio)).EndInit();
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconTitulo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -343,8 +421,12 @@
         private FontAwesome.Sharp.IconButton BtnRam;
         private FontAwesome.Sharp.IconButton BtnClientes;
         private FontAwesome.Sharp.IconButton BtnProcesadores;
-        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox BtnInicio;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel gunaPanelFondo;
+        private System.Windows.Forms.Label lblTitulo;
+        private FontAwesome.Sharp.IconPictureBox iconTitulo;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
     }
 }
 
--- BaseDeDatosBOA/Form1.cs
+++ BaseDeDatosBOA/Form1.cs
@@ -64,30 +64,30 @@ namespace BaseDeDatosBOA
                 resaltarBoton();
                 //Boton
                 currentbtn = (IconButton)senderBtn;
-                currentbtn.BackColor = Color.FromArgb(12, 7, 21);
+                currentbtn.BackColor = Color.FromArgb(61, 89, 97);
                 currentbtn.ForeColor = Color.White;
                 currentbtn.TextAlign = ContentAlignment.MiddleCenter;
-                currentbtn.IconColor = Color.FromArgb(212, 55, 77);
+                currentbtn.IconColor = Color.FromArgb(4, 35, 55);
                 currentbtn.TextImageRelation = TextImageRelation.TextBeforeImage;
                 currentbtn.ImageAlign = ContentAlignment.MiddleRight;
                 //bordeIzquierdobtn
-                bordeIzquierdobtn.BackColor = Color.FromArgb(212, 55, 77);
+                bordeIzquierdobtn.BackColor = Color.FromArgb(4, 35, 55);
                 bordeIzquierdobtn.Location = new Point(0, currentbtn.Location.Y);
                 bordeIzquierdobtn.Visible = true;
                 bordeIzquierdobtn.BringToFront();
                 //Icono titulo hijo form
                 iconTitulo.IconChar = currentbtn.IconChar;
-                iconTitulo.IconColor = Color.FromArgb(212, 55, 77);
+                iconTitulo.IconColor = Color.FromArgb(4, 35, 55);
             }
         }
         private void resaltarBoton()
         {
             if (currentbtn != null)
             {
-                currentbtn.BackColor = Color.FromArgb(12, 7, 21);
+                currentbtn.BackColor = Color.FromArgb(61, 89, 97);
                 currentbtn.ForeColor = Color.White;
                 currentbtn.TextAlign = ContentAlignment.MiddleLeft;
-                currentbtn.IconColor = Color.FromArgb(212, 55, 77);
+                currentbtn.IconColor = Color.FromArgb(4, 35, 55);
                 currentbtn.TextImageRelation = TextImageRelation.ImageBeforeText;
                 currentbtn.ImageAlign = ContentAlignment.MiddleLeft;
 
@@ -107,7 +107,7 @@ namespace BaseDeDatosBOA
             resaltarBoton();
             bordeIzquierdobtn.Visible = false;
             iconTitulo.IconChar = IconChar.Home;
-            iconTitulo.IconColor = Color.FromArgb(212, 55, 77);
+            iconTitulo.IconColor = Color.FromArgb(4, 35, 55);
             lblTitulo.Text = "Inicio";
         }
         //Form
@@ -118,7 +118,7 @@ namespace BaseDeDatosBOA
         private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
         private void Form1_Load(object sender, EventArgs e)
         {
-            LoadData();
+            //LoadData();
         }
 
         private void LoadData()
@@ -126,7 +126,7 @@ namespace BaseDeDatosBOA
             try
             {
                 List<Computadora> computadoras = logica.ObtenerComputadoras();
-                dataGridView1.DataSource = computadoras;
+                //dataGridView1.DataSource = computadoras;
             }
             catch (Exception ex)
             {
@@ -137,68 +137,88 @@ namespace BaseDeDatosBOA
         private void BtnVenta_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Ventas());
         }
 
         private void BtnInventario_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Inventario());
         }
 
         private void BtnClientes_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Clientes());
         }
 
         private void BtnEmpleados_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Empleados());
         }
 
         private void BtnComputadoras_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Computadoras());
         }
 
         private void BtnTarjetaMadre_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Tarjetas_Madre());
         }
 
         private void BtnProcesadores_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Procesadores());
         }
 
         private void BtnGrafica_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Graficas());
         }
 
         private void BtnRam_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new RAM());
 
         }
 
         private void BtnAlmacenamiento_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Almacenamiento());
         }
 
         private void BtnFuenteDePoder_Click(object sender, EventArgs e)
         {
             ActivarBoton(sender, almacenarRGB.color1);
-            formularioHijo(new Figura1());
+            formularioHijo(new Fuentes_De_Poder());
+        }
+
+        private void BtnInicio_Click_1(object sender, EventArgs e)
+        {
+            if (formHijo != null)
+            {
+                //Al abrir un formulario cerramos el formulario anterior
+                formHijo.Close();
+                Reset();
+            }
+        }
+
+        private void guna2Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture(); SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
